094571e84fee8faa263160e3230afda3049068f9
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-04-06 17:55:32 +0200

    generified work item

--- kie-api/src/main/java/org/kie/api/runtime/process/WorkItemHandler.java
+++ kie-api/src/main/java/org/kie/api/runtime/process/WorkItemHandler.java
@@ -23,11 +23,11 @@ package org.kie.api.runtime.process;
  * and the implementation of this work item.  Work item handlers
  * should be registered with the <code>WorkItemManager</code> for
  * each type of work that can be executed in the engine.
- *
+ * <p>
  * A work item handler is responsible for executing a work item whenever
  * the work item manager delegates one to it.  It should also notify
  * the work item manager when the work item has been completed.
- *
+ * <p>
  * It is also possible that a work item handler is requested to abort
  * an existing work item (that is still executing) because it is no longer
  * necessary.  This might for example be because the process instance
@@ -35,20 +35,20 @@ package org.kie.api.runtime.process;
  * aborted.  The work item handler should then try to abort this work item
  * (if possible) and if necessary clean up runtime state related to its
  * execution.
- *
+ * <p>
  * For example, a work item handler that is responsible for executing
  * email work items will retrieve the necessary information from the
  * work item (from, to, body, etc.) and invoke the mail server.  Afterwards,
  * it will notify the <code>WorkItemManager</code> that the work item was completed.
  */
-public interface WorkItemHandler {
+public interface WorkItemHandler<W extends WorkItem<?, ?>> {
 
     /**
      * The given work item should be executed.
      * @param workItem the work item that should be executed
      * @param manager the manager that requested the work item to be executed
      */
-    void executeWorkItem(WorkItem workItem,
+    void executeWorkItem(W workItem,
                          WorkItemManager manager);
 
     /**
@@ -56,7 +56,6 @@ public interface WorkItemHandler {
      * @param workItem the work item that should be aborted
      * @param manager the manager that requested the work item to be aborted
      */
-    void abortWorkItem(WorkItem workItem,
+    void abortWorkItem(W workItem,
                        WorkItemManager manager);
-
 }
